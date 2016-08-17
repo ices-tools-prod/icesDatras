dccd64edb20de7b70cd0002d79d5e4c5a7edb1e5
Arni Magnusson <arnima@hafro.is>
2016-08-17 23:55:09 +0200

    Update links on package help page

--- R/icesDatras-package.R
+++ R/icesDatras-package.R
@@ -4,23 +4,19 @@
 #'
 #' @aliases icesDatras
 #'
-#' @title DATRAS trawl database webservices
+#' @title DATRAS Trawl Database Web Services
 #'
 #' @description
-#' Functions that access the webservices of the ICES (International
+#' Functions that access the web services of the ICES (International
 #' Council for the Exploration of the Sea) DATRAS trawl database.
 #'
 #' @author Colin Millar, Einar Hjorleifsson, Scott Large, and Arni Magnusson.
 #'
 #' @references
-#' Overview of the ICES advisory process:
-#' \url{http://ices.dk/community/advisory-process/Pages/default.aspx}.
+#' Description of ICES DATRAS web services:
+#' \url{https://datras.ices.dk/WebServices/Webservices.aspx}.
 #'
 #' ICES list of software applications:
 #' \url{http://ices.dk/marine-data/tools/Pages/Software.aspx}.
-#'
-#' ICES Datras Webservices:
-#' \url{https://datras.ices.dk/WebServices/Webservices.aspx}.
-#'
 
 NA
