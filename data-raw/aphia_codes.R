eff8a54985a085e728079ada9a9ecf5a5795a167
colin millar <colinpmillar@gmail.com>
2016-09-14 14:36:46 +0200

    trim white space from species names in aphia data

--- data-raw/aphia_codes.R
+++ data-raw/aphia_codes.R
@@ -6,5 +6,8 @@ aphia <- read.csv("data-raw/RECO_Export_14-59-2016.csv")
 aphia <- aphia[c("Code", "Description")]
 names(aphia) <- c("aphia_code", "species")
 
+# trim white space
+aphia$species <- trimws(aphia$species)
+
 # save to data folder
-devtools::use_data(aphia)
+devtools::use_data(aphia, overwrite = TRUE)
