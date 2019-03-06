9653d60fe8832955e0e144fb1f66bd8f8caeb74f
Arni Magnusson <arnima@hafro.is>
2019-03-06 15:11:37 +0100

    Document existence of getCatchWgt() and getIndices(), restructure overview

--- R/icesDatras-package.R
+++ R/icesDatras-package.R
@@ -11,12 +11,20 @@
 #' database.
 #'
 #' @details
-#' \emph{Get dataset:}
+#' \emph{Exchange data:}
 #' \tabular{ll}{
 #'   \code{\link{getHHdata}} \tab haul data\cr
 #'   \code{\link{getHLdata}} \tab length-based data\cr
 #'   \code{\link{getCAdata}} \tab age-based data\cr
-#'   \code{\link{getDATRAS}} \tab any data
+#'   \code{\link{getDATRAS}} \tab exchange data
+#' }
+#' \emph{Catch weights:}
+#' \tabular{ll}{
+#'   \code{\link{getCatchWgt}} \tab catch weights
+#' }
+#' \emph{Survey indices:}
+#' \tabular{ll}{
+#'   \code{\link{getIndices}} \tab survey indices
 #' }
 #' \emph{Overview of available data:}
 #' \tabular{ll}{
@@ -25,13 +33,19 @@
 #'   \code{\link{getSurveyYearQuarterList}} \tab quarters\cr
 #'   \code{\link{getDatrasDataOverview}}    \tab surveys, years, and quarters
 #' }
-#' \emph{Calculate:}
-#' \tabular{ll}{
-#'   \code{\link{getSurveyList}}            \tab surveys\cr
-#'   \code{\link{getSurveyYearList}}        \tab years\cr
-#'   \code{\link{getSurveyYearQuarterList}} \tab quarters\cr
-#'   \code{\link{getDatrasDataOverview}}    \tab surveys, years, and quarters
-#' }
+#' \emph{Basic queries (thin web service wrappers):}\cr
+#' \code{getCAdata},
+#' \code{getHHdata},
+#' \code{getHLdata},
+#' \code{getIndices},
+#' \code{getSurveyList},
+#' \code{getSurveyYearList},
+#' \code{getSurveyYearQuarterList}
+#'
+#' \emph{Derived queries (combining web services with R computations):}\cr
+#' \code{getCatchWgt},
+#' \code{getDatrasDataOverview},
+#' \code{getDATRAS}
 #'
 #' @author Colin Millar, Scott Large, and Arni Magnusson.
 #'
@@ -40,6 +54,5 @@
 #'
 #' ICES DATRAS web services:
 #' \url{https://datras.ices.dk/WebServices/Webservices.aspx}.
-
 
 NA
