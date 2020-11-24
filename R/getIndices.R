77a62b02872b1ba5246bab8806bb710ac9cf6080
Arni Magnusson <arni.magnusson@ices.dk>
2020-11-25 00:41:22 +0100

    Mention that icesVocab provides findAphia()

--- R/getIndices.R
+++ R/getIndices.R
@@ -1,22 +1,18 @@
 #' Get Survey Indices
 #'
-#' Get age based indices of abundance by species, survey and year.
+#' Get age-based indices of abundance by species, survey and year.
 #'
 #' @param survey the survey acronym, e.g. NS-IBTS.
 #' @param year the year of the survey, e.g. 2010.
 #' @param quarter the quarter of the year the survey took place, i.e. 1, 2, 3 or 4.
-#' @param species the aphia species code for the species of interest.
+#' @param species the Aphia species code for the species of interest.
 #'
 #' @return A data frame.
+#'
 #' @note
-#' The \pkg{icesAdvice} package provides \code{findAphia}, a function to look up Aphia species codes.
+#' The \pkg{icesVocab} package provides \code{findAphia}, a function to look up Aphia species codes.
 #'
 #' @seealso
-#' \code{\link{getDATRAS}} supports querying many years and quarters in one function call.
-#'
-#' \code{\link{getHHdata}} and \code{\link{getCAdata}} get haul data and
-#' age-based data.
-#'
 #' \code{\link{icesDatras-package}} gives an overview of the package.
 #'
 #' @examples
@@ -25,6 +21,7 @@
 #' index <- getIndices(survey = "NS-IBTS", year = 2002, quarter = 3, species = haddock_aphia)
 #' str(index)
 #' }
+#'
 #' @export
 
 getIndices <- function(survey, year, quarter, species) {
